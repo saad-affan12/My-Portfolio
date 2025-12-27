e0ba73ef67ea00314b976f77c0ac3906000a34eb
MOHAMMED SAAD AFFAN A <saadaffan129@gmail.com>
2025-12-27 20:05:27 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,13 @@
-document.documentElement.style.scrollBehavior = "smooth";
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import "./index.css";
+
+/**
+ * Global smooth scroll (native, performant)
+ * Premium feel, no overengineering
+ */
+document.documentElement.style.scrollBehavior = "smooth";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
